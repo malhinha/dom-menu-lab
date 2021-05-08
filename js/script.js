dc0ea91570bfe5faa043a9ffd34ad76fa0ed980c
John Malhinha <john.malhinha@gmail.com>
2021-05-08 11:23:12 -0700

    Task 1 complete

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Task 1.0
+const mainEl = document.querySelector('main');
+
+// Task 1.1
+let color = 'var(--main-bg)';
+mainEl.style.background = color;
+
+// Task 1.2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+
+// Task 1.3
+mainEl.classList.add('flex-ctr');
+
+// Task 2.0
+const topMenuEl = document.getElementById('top-menu');
+
+// debug
+console.log(mainEl);
