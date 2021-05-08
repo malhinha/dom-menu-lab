ad7c890399df0a7ee9494c77f150add2b2ad3d9c
John Malhinha <john.malhinha@gmail.com>
2021-05-08 11:40:36 -0700

    Task 2 complete

--- js/script.js
+++ js/script.js
@@ -14,5 +14,15 @@ mainEl.classList.add('flex-ctr');
 // Task 2.0
 const topMenuEl = document.getElementById('top-menu');
 
+// Task 2.1
+topMenuEl.style.height = '100%';
+
+// Task 2.2
+topMenuEl.style.background = 'var(--top-menu-bg)';
+
+// Task 2.3
+topMenuEl.classList.add('flex-around');
+
 // debug
 console.log(mainEl);
+console.log(topMenuEl);
